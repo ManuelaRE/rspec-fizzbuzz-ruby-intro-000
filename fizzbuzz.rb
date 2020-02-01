7efca3a2f669c0ec9fc84f96f837b9c496c5194a
Manuela Redinciuc <manutzu15@gmail.com>
2020-02-01 11:48:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,13 @@
 def fizzbuzz(number)
   if number % 3 == 0
     return "Fizz"
+  end
   elsif number % 5 == 0
     return "Buzz"
-  elsif number (% 3 == 0 && % 5 == 0)
+  end
+  elsif number % 3 == 0 && % 5 == 0
   return "FizzBuzz"
+end
 else return nil
 end
 end
